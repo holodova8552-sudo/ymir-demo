3bed58950dae9a79a39fd5dcedee692c3bfd4e90
holodova8552-sudo <holodova8552@gmail.com>
2025-12-07 00:50:47 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/login");
+  }, []);
+  return null;
+}
