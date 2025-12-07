9eb63b549394cd19517a47e15df5eaee5a717b83
holodova8552-sudo <holodova8552@gmail.com>
2025-12-07 12:29:52 +0200

    Add files via upload

--- pages/api/shop/items.ts
+++ pages/api/shop/items.ts
@@ -1,12 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-/*
-Static catalog for demo. Place images in /public/images/shop/
-*/
 const items = [
   { id: "p1", name: "ODM Gas Canister", price: 100, image: "/images/shop/gas.png" },
   { id: "p2", name: "Scout Cloak", price: 250, image: "/images/shop/cloak.png" },
-  { id: "p3", name: "Titan Serum", price: 1000000, image: "/images/shop/serum.png" }
 ];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
